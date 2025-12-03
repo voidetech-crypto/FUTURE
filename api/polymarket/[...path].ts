facdf47e00f570cc50d25e47836cec42ea87b211
voidetech-crypto <voidetech@gmail.com>
2025-12-03 18:21:45 -0500

    Fix TypeScript import error - add .js extension

--- api/polymarket/[...path].ts
+++ api/polymarket/[...path].ts
@@ -1,7 +1,7 @@
 // Vercel API Route - handles all /api/polymarket/* requests
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import polymarketRouter from "../../src/worker/api/polymarket";
+import polymarketRouter from "../../src/worker/api/polymarket.js";
 
 const app = new Hono();
 
